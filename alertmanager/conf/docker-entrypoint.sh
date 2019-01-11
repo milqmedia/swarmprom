250e3c0c9a0ad2d038db9bc50d3063880b2d39f2
Yo-han <j.kuijt@wintersport.nl>
2019-01-11 16:58:31 +0100

    start milqifying the stack

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -1,9 +1,11 @@
 #!/bin/sh -e
 
 cat /etc/alertmanager/alertmanager.yml |\
-    sed "s@#api_url: <url>#@api_url: '$SLACK_URL'@g" |\
-    sed "s@#channel: <channel>#@channel: '#$SLACK_CHANNEL'@g" |\
-    sed "s@#username: <user>#@username: '$SLACK_USER'@g" > /tmp/alertmanager.yml
+    sed "s@#smtp_smarthost: <host>#@smtp_smarthost: '$SMTP_HOST'@g" |\
+    sed "s@#smtp_from: <from>#@smtp_from: '#$SMTP_FROM'@g" |\
+    sed "s@#smtp_auth_username: <user>#@smtp_auth_username: '#$SMTP_USER'@g" |\
+    sed "s@#smtp_auth_password: <password>#@smtp_auth_password: '$SMTP_PASSWORD'@g" |\
+    sed "s@#to: <emails>#@to: '$ALERT_EMAILS'@g" > /tmp/alertmanager.yml
 
 mv /tmp/alertmanager.yml /etc/alertmanager/alertmanager.yml
 
--- node-exporter/conf/docker-entrypoint.sh
+++ node-exporter/conf/docker-entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh -e
 
 NODE_NAME=$(cat /etc/nodename)
+SERVER_CONFIG=$(cat /etc/milq/server.env)
+
 echo "node_meta{node_id=\"$NODE_ID\", container_label_com_docker_swarm_node_id=\"$NODE_ID\", node_name=\"$NODE_NAME\"} 1" > /etc/node-exporter/node-meta.prom
+echo "node_env{env=\"$SERVER_CONFIG\"} 1" > /etc/node-exporter/node-env.prom
 
 set -- /bin/node_exporter "$@"
 
