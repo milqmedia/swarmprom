da5a463440466962a3814e43c13240d429d835bc
Yo-han <j.kuijt@wintersport.nl>
2019-01-14 11:30:26 +0100

    fix

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -2,8 +2,8 @@
 
 cat /etc/alertmanager/alertmanager.yml |\
     sed "s@#smtp_smarthost: <host>#@smtp_smarthost: '$SMTP_HOST'@g" |\
-    sed "s@#smtp_from: <from>#@smtp_from: '#$SMTP_FROM'@g" |\
-    sed "s@#smtp_auth_username: <user>#@smtp_auth_username: '#$SMTP_USER'@g" |\
+    sed "s@#smtp_from: <from>#@smtp_from: '$SMTP_FROM'@g" |\
+    sed "s@#smtp_auth_username: <user>#@smtp_auth_username: '$SMTP_USER'@g" |\
     sed "s@#smtp_auth_password: <password>#@smtp_auth_password: '$SMTP_PASSWORD'@g" |\
     sed "s@#to: <emails>#@to: '$ALERT_EMAILS'@g" > /tmp/alertmanager.yml
 
