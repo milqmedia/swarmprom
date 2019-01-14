c6e56c7428da13d08fef23bd9f5bd329798278a7
Yo-han <j.kuijt@wintersport.nl>
2019-01-14 13:08:18 +0100

    working alertmanager setup

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -1,10 +1,10 @@
 #!/bin/sh -e
 
 cat /etc/alertmanager/alertmanager.yml |\
-    sed "s@#smtp_smarthost: <host>#@smtp_smarthost: '$SMTP_HOST'@g" |\
-    sed "s@#smtp_from: <from>#@smtp_from: '$SMTP_FROM'@g" |\
-    sed "s@#smtp_auth_username: <user>#@smtp_auth_username: '$SMTP_USER'@g" |\
-    sed "s@#smtp_auth_password: <password>#@smtp_auth_password: '$SMTP_PASSWORD'@g" |\
+    sed "s@#smarthost: <host>#@smarthost: '$SMTP_HOST'@g" |\
+    sed "s@#from: <from>#@from: '$SMTP_HOST'@g" |\
+    sed "s@#auth_username: <user>#@auth_username: '$SMTP_USER'@g" |\
+    sed "s@#auth_password: <password>#@auth_password: '$SMTP_PASSWORD'@g" |\
     sed "s@#to: <emails>#@to: '$ALERT_EMAILS'@g" > /tmp/alertmanager.yml
 
 mv /tmp/alertmanager.yml /etc/alertmanager/alertmanager.yml
