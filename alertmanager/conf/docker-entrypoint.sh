db9e57b718d9a9735f630d9ea46551d8189e9cc8
Yo-han <j.kuijt@wintersport.nl>
2019-01-15 10:26:28 +0100

    typo

--- alertmanager/conf/docker-entrypoint.sh
+++ alertmanager/conf/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 cat /etc/alertmanager/alertmanager.yml |\
     sed "s@#smarthost: <host>#@smarthost: '$SMTP_HOST'@g" |\
-    sed "s@#from: <from>#@from: '$SMTP_HOST'@g" |\
+    sed "s@#from: <from>#@from: '$SMTP_FROM'@g" |\
     sed "s@#auth_username: <user>#@auth_username: '$SMTP_USER'@g" |\
     sed "s@#auth_password: <password>#@auth_password: '$SMTP_PASSWORD'@g" |\
     sed "s@#to: <emails>#@to: '$ALERT_EMAILS'@g" > /tmp/alertmanager.yml
